e188e7911313560b638d5a2b5e707849e20ee5d5
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2020-08-24 16:50:29 +0530

    1348A- Phoenix and Balance

---
+++ src/MishkaAndGame.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MishkaAndGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        int sum1=0;
+        int sum2=0;
+        for (int i = 0; i < n ; i++) {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a<b){
+                sum2++;
+            } else if(a>b){
+                sum1++;
+            } else if (a==b){
+                continue;
+            }
+        }
+        if (sum1>sum2){
+            System.out.println("Mishka");
+        } else if(sum2>sum1){
+            System.out.println("Chris");
+        } else if (sum1==sum2){
+            System.out.println("Friendship is magic!^^");
+        }
+    }
+}
