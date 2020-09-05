496d9fb5c420aadbf63a2be91a603a67fbff285d
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2020-09-05 14:34:54 +0530

    Football

---
+++ src/Football.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Football {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+            if (s.contains("0000000") || s.contains("1111111")){
+                System.out.println("YES");
+            } else{
+                System.out.println("NO");
+            }
+        }
+
+    }
+
