b16e24dc8016d3b3f01be21b8234c7d020e08577
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2020-09-21 15:59:37 +0530

    1248B Grow the tree.java

--- src/GrowTheTree1248.java
+++ src/GrowTheTree1248.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 public class GrowTheTree1248 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         sc.nextLine();
-        int arr[]=new int[n];
-        for (int i = 0; i < n ; i++) {
-            arr[i]=sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        long sum=0;
-        long sum2=0;
-        if (n%2==0){
-            for (int i = 0; i < (n/2) ; i++) {
-                sum=sum+arr[i];
+            Arrays.sort(arr);
+            long sum = 0;
+            long sum2 = 0;
+            if (n % 2 == 0) {
+                for (int i = 0; i < (n / 2); i++) {
+                    sum = sum + arr[i];
+                }
+                for (int i = n / 2; i < n; i++) {
+                    sum2 = sum2 + arr[i];
+                }
+            } else {
+                for (int i = 0; i < (n / 2); i++) {
+                    sum = sum + arr[i];
+                }
+                for (int i = n / 2; i < n; i++) {
+                    sum2 = sum2 + arr[i];
+                }
             }
-            for (int i = n/2; i < n ; i++) {
-                sum2=sum2+arr[i];
-            }
-        } else {
-            for (int i = 0; i < (n/2) ; i++) {
-                sum=sum+arr[i];
-            }
-            for (int i = n/2; i < n ; i++) {
-                sum2=sum2+arr[i];
-            }
-        }
-        System.out.println((sum*sum)+(sum2*sum2));
+            System.out.println((sum * sum) + (sum2 * sum2));
     }
 }
