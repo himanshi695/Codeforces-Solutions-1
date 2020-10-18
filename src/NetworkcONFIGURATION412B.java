684671410acaf603dfdd51ec2aeda6f1a84b0245
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2020-10-18 11:21:37 +0530

    412B- Network Configuration

---
+++ src/NetworkcONFIGURATION412B.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NetworkcONFIGURATION412B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.nextLine();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[n-k]);
+    }
+}
